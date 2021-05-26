390a98a14b6b6d182262efa21b5e79f94db3f5e1
Vishal Nair <vishal.nair@delphix.com>
2021-05-26 21:51:23 +0530

    Initial commit.

---
+++ src/main/java/org/upgrad/upstac/users/User.java
@@ -0,0 +1,89 @@
+package org.upgrad.upstac.users;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.upgrad.upstac.users.models.AccountStatus;
+import org.upgrad.upstac.users.models.Gender;
+import org.upgrad.upstac.users.roles.Role;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(unique = true)
+  private String userName;
+
+  @Column @JsonIgnore @ToString.Exclude private String password;
+
+  private LocalDateTime created;
+
+  private LocalDate dateOfBirth;
+
+  private LocalDateTime updated;
+
+  private String firstName;
+
+  private AccountStatus status;
+
+  @Column(unique = true)
+  private String email;
+
+  private String lastName;
+
+  private Gender gender;
+
+  @Column(unique = true)
+  private String phoneNumber;
+
+  private String address;
+
+  private Integer pinCode;
+
+  @ManyToMany(fetch = FetchType.EAGER)
+  private Set<Role> roles;
+
+  public boolean doesRoleIsDoctor() {
+
+    return doesUserHasRole("DOCTOR");
+  }
+
+  public boolean doesUserHasRole(String s) {
+    return roles.stream()
+        .filter(
+            role -> {
+              return role.getName().equalsIgnoreCase(s);
+            })
+        .findFirst()
+        .isPresent();
+  }
+
+  public boolean doesRoleIsUser() {
+    return doesUserHasRole("USER");
+  }
+
+  public boolean doesRoleIsAuthority() {
+    return doesUserHasRole("GOVERNMENT_AUTHORITY");
+  }
+
+  public boolean doesRoleIsTester() {
+    return doesUserHasRole("TESTER");
+  }
+
+  public Integer getAge() {
+    if (null != dateOfBirth) return LocalDate.now().getYear() - dateOfBirth.getYear();
+    else return 0;
+  }
+}
